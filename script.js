7497a4e8c060a177c30cdd1860271a0e6e8d01fa
Sahil Sheikh <153609223+sahil-sheikh01@users.noreply.github.com>
2026-05-28 13:46:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwufdN1yUVl0s_2kIzLJai4b4S_sgdZGSg6qIt168ATZVI5hr_58-sYCOQKrCffSkXvKg/exec';
+const form = document.forms['subscription'];
+const msg = document.getElementById('msg');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    msg.innerHTML = "Subscribing...";
+
+    fetch(scriptURL, {
+        method: 'POST', 
+        body: new FormData(form) 
+    })
+    .then(() => {
+        msg.innerHTML = "Thank you for Subscribing :)";
+
+        setTimeout(function(){
+            msg.innerHTML = "";
+        }, 5000);
+
+        form.reset();
+    })
+    .catch(error => {
+        msg.innerHTML = "Something went wrong :("
+        console.error('Error!', error.message)
+    });
+});
